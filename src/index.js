c9ecbbcf3200ef1d02cd01d390d4a85c6b0e280b
Ilya_Murashko <70842990+murashkoIl@users.noreply.github.com>
2021-08-21 19:17:26 +0300

    feat: added implementation method

--- src/index.js
+++ src/index.js
@@ -37,8 +37,28 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const CODE_DOT = '10';
+const CODE_DASH = '11'
+const SYMBOL_LENGTH = 2;
+const LETTER_LENGTH = 10;
+
 function decode(expr) {
-    // write your solution here
+    let arr = expr.split("");
+    let resultArr = [];
+    while (arr.length) {
+        let ten = arr.splice(LETTER_LENGTH * -1);
+        let morseLetter = "";
+        if (ten.join("") === "**********") {
+            resultArr.push(" ");
+            continue;
+        }
+        for (i = 0; i < ten.length; i += SYMBOL_LENGTH) {
+            let signCode = ten.slice(i, i + SYMBOL_LENGTH).join("");
+            morseLetter += signCode === CODE_DOT ? "." : signCode === CODE_DASH ? "-" : "";
+        }
+        resultArr.push(MORSE_TABLE[morseLetter]);
+    }
+    return resultArr.reverse().join("");
 }
 
 module.exports = {
